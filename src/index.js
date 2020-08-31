41098fa01b58438811e1c172d0406f4fba859334
Mikhail Kukushkin <qq6kin87@gmail.com>
2020-08-31 17:41:18 +0300

    header end

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import './style.css'
+import './style.css';
+
+
+(function () {
+  const burgerBtn = document.querySelector('.burger-menu');
+  
+  function toggleHandler(toggle) {
+    burgerBtn.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  };
+  })();
