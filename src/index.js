d5325d765457dd0f40396da6ef90994b94750648
Mikhail Kukushkin <qq6kin87@gmail.com>
2020-08-31 21:50:26 +0300

    footer slider collection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import './style.css';
+// import './style.css';
 
 
-(function () {
-  const burgerBtn = document.querySelector('.burger-menu');
+// (function () {
+//   const burgerBtn = document.querySelector('.burger-menu');
   
-  function toggleHandler(toggle) {
-    burgerBtn.addEventListener( "click", function(e) {
-      e.preventDefault();
-      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
-    });
-  };
-  })();
+//   function toggleHandler(toggle) {
+//     burgerBtn.addEventListener( "click", function(e) {
+//       e.preventDefault();
+//       (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+//     });
+//   };
+//   })();
+
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:1,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
